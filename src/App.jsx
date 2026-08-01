7d5136058d6a289794423d70c11f03e72ae101f2
Abdurrahman Muhammad Bello <133776530+Abdulrahmanvisit@users.noreply.github.com>
2026-08-01 15:26:22 +0100

    Add responsive navbar with mobile menu toggle using useState

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { Routes, Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
+
+
+function Home() {
+  return <h1>Home Page Placeholder</h1>
+}
+
+function Search() {
+  return <h1>Search Page Placeholder</h1>
+}
+
+function About() {
+  return <h1>Return About Page Placeholder.</h1>
+}
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/About" element={<About />}></Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
